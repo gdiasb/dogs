b8dbbed7e0a0f2466e1515b23d053faf885314e7
Gabriela <gabrieladias@eq.ufrj.br>
2021-12-21 12:14:52 -0300

    Limpeza ex_1

--- src/ex_1.js
+++ src/ex_1.js
@@ -1,24 +1,3 @@
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  }
-];
-
 const luana = {
   cliente: 'Luana',
   idade: 27,
@@ -46,6 +25,8 @@ function App() {
   
   const dados = mario;
 
+  console.log(luana);
+
   // console.log(dados.compras.filter((dado) => dado.preco.includes('R$')));
 
   // console.log(dados.compras.map((item) => Number(item.preco.replace('R$ ', ''))));
@@ -54,44 +35,21 @@ function App() {
   .map((item) => Number(item.preco.replace('R$ ', '')))
   .reduce((a, b) => a+b)
 
-  produtos.map((dado) => Number(dado.preco.replace('R$ ', '')))
-  console.log(produtos)
-
-
   // console.log(produtos.map((item) => item.cores))
   // console.log(produtosTratados)
 
-
   return(
     // {Exercício 1}
-    // <div>
-    //   <p>Cliente: {dados.cliente}</p>      
-    //   <p>Idade: {dados.idade}</p>
-    //   <p>Total: R$ {total}</p>
-    //   <p>Situação:
-    //     <span style={{color: total > 10000 ? 'red' : 'green'}}>  
-    //       {dados.ativa ? ' Ativa' : ' Inativa'}
-    //     </span>
-    //   </p>
-    //   <p>{total>10000 && 'Você está gastando demais.'}</p>
-    // </div>
-
-    // {Exercício 2}
-
     <div>
-      {produtos
-      .map((dado) => 
-      <>
-      <h1 key={dado.id}>{dado.nome}</h1>
-      <p>Preço: {dado.preco}</p>
-      <ul>
-        {/* <li key={dado.id} style={{background: produtos.map((item) => item.cores)}}>{produtos.map((item) => item.cores)}</li> */}
-        {produtos.map((item) => 
-        <p></p>)}
-      </ul>
-      </>
-      
-      )}
+      <p>Cliente: {dados.cliente}</p>      
+      <p>Idade: {dados.idade}</p>
+      <p>Total: R$ {total}</p>
+      <p>Situação:
+        <span style={{color: total > 10000 ? 'red' : 'green'}}>  
+          {dados.ativa ? ' Ativa' : ' Inativa'}
+        </span>
+      </p>
+      <p>{total>10000 && 'Você está gastando demais.'}</p>
     </div>
   );
 }
