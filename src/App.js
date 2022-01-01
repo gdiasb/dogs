80462d64136bd33726d0b10e9891e93cde4d5e15
Gabriela <gabrieladias@eq.ufrj.br>
2022-01-01 19:49:14 -0300

    Separação de Projetos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import './styles.css';
 
 function App() {
+  const [data, setData] = React.useState([]);
+  const [selectedProduct, setSelectedProduct] = React.useState(null);
+  let active = false;
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  function handleClick(event) {
+    active = !active;
+    setSelectedProduct(
+      data.find((item) => item.id === event.target.getAttribute('produto-id')),
+    );
+  }
+
   return (
-  <div>
-    <h1>oi</h1>
-  </div>
+    <div className="grid">
+      <div className="grid_left_column">
+        {data.map((produto) => (
+          <div className="grid_min_card" key={produto.id}>
+            <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+            <button produto-id={produto.id} onClick={handleClick}>
+              {produto.nome}
+            </button>
+          </div>
+        ))}
+      </div>
+      {/* <div className="grid_right_column">{!active && <h1>{selectedProduct.nome}</h1>}</div> */}
+    </div>
   );
 }
 
